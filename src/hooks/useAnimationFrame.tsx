a35590dba0f8d4d4d7cc4cac44b3a3b32a371bc2
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2024-06-04 21:48:48 -0700

    add custom animation hook

---
+++ src/hooks/useAnimationFrame.tsx
@@ -0,0 +1,61 @@
+import { useRef, useEffect, useState, useCallback } from "react";
+
+const useAnimationFrame = (callback: () => void, perMinuteRate: number) => {
+  const callbackRef = useRef(callback);
+  const frameRef = useRef<number | null>(null);
+  const lastExecutionTimeRef = useRef<number>(0);
+  const interval = 60000 / perMinuteRate; 
+
+  const [isRunning, setIsRunning] = useState(false);
+
+  useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  const loop = useCallback(
+    (time: number) => {
+      if (time - lastExecutionTimeRef.current >= interval) {
+        callbackRef.current();
+        lastExecutionTimeRef.current = time;
+      }
+      if (isRunning) {
+        frameRef.current = requestAnimationFrame(loop);
+      }
+    },
+    [interval, isRunning]
+  );
+
+  const start = useCallback(() => {
+    if (!isRunning) {
+      setIsRunning(true);
+      lastExecutionTimeRef.current = performance.now();
+    }
+  }, [isRunning]);
+
+  const stop = useCallback(() => {
+    setIsRunning(false);
+    if (frameRef.current !== null) {
+      cancelAnimationFrame(frameRef.current);
+      frameRef.current = null;
+    }
+  }, []);
+
+  useEffect(() => {
+    if (isRunning) {
+      frameRef.current = requestAnimationFrame(loop);
+    }
+  }, [isRunning, loop]);
+
+  useEffect(() => {
+    return () => {
+      if (frameRef.current !== null) {
+        console.log("cancelling from unmount");
+        cancelAnimationFrame(frameRef.current);
+      }
+    };
+  }, []);
+
+  return { start, stop };
+};
+
+export default useAnimationFrame;

f726c05ea4b4752424f092a898c4b0210f1a9223
Matt Lane <3916770+mmmaaatttttt@users.noreply.github.com>
2024-06-09 14:02:56 -0700

    load animation provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,12 +5,15 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import SettingsProvider from "providers/settings-provider.tsx";
+import SettingsProvider from "providers/settings-provider";
+import AnimationProvider from "providers/animation-provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <SettingsProvider>
-      <App />
+      <AnimationProvider>
+        <App />
+      </AnimationProvider>
     </SettingsProvider>
   </React.StrictMode>,
 );
